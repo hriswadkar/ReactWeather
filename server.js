69f35a8cd448661f00bcddec75baa5ed1d6459b8
hriswadkar <riswadkar.harshad@gmail.com>
2018-01-20 21:07:48 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 
 app.listen(port, function () {
     console.log("Express server is up on port " + PORT);
