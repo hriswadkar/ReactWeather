6c8602925a7b7261124d15f5aa837f6ea2f6bd4e
hriswadkar <riswadkar.harshad@gmail.com>
2018-01-21 18:05:44 +0530

    Updated code for error but not working so far

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,25 +1,25 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/find?q=';
-const REMAINING_URL = '&APPID=94090fac0e474b13059afa238d600565&units=metric';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=94090fac0e474b13059afa238d600565&units=metric';
 
 // 7543332926dd68132bfd44ae39d13469
 
 module.exports = {
     getTemp: function (location) {
         var encodedLocation = encodeURIComponent(location);
-        //var requestUrl = `${OPEN_WEATHER_MAP_URL}${encodedLocation}&`;
-        var requestUrl = OPEN_WEATHER_MAP_URL + encodedLocation + REMAINING_URL;
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+        //var requestUrl = OPEN_WEATHER_MAP_URL + encodedLocation + REMAINING_URL;
 
         return axios.get(requestUrl).then(function (res) {
             debugger;
-            if (res.data.cod && (res.data.message != "accurate")) {
+            if (res.data.cod && res.data.message) {
                 throw new Error(res.data.message);
             } else {
-                return res.data.list[0].main.temp;
+                return res.data.main.temp;
             }
         }, function (res) {
-            throw new Error(res.data.message);
+            const response = res.response;
+            throw new error(response.data.message);
         });
     }
 }
